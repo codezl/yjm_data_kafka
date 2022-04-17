dec7ae80d98d9723c06eb50a30a7942eb6a458f3
18871127641@163.com <2859215356>
2022-04-17 19:23:47 +0800

    2022-04-17

--- src/main/java/com/zcdl/yjm_data_kafka/config/SwaggerConfig.java
+++ src/main/java/com/zcdl/yjm_data_kafka/config/SwaggerConfig.java
@@ -2,7 +2,6 @@ package com.zcdl.yjm_data_kafka.config;
 
 import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
 import io.swagger.annotations.Api;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -14,32 +13,40 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
- * Created by Mata on 2020/4/15.
+ * Created by Administrator on 2018/12/5.
  * Use to
  */
+@Configuration
 @EnableSwagger2
 @EnableSwaggerBootstrapUI
-@Configuration
-@ConditionalOnProperty(name = "swagger.enable", havingValue = "true")
 public class SwaggerConfig {
+    //是否开启swagger，正式环境一般是需要关闭的，可根据springboot的多环境配置进行设置
+//    @Value("${swagger.enabled}")
+//    Boolean swaggerEnabled;
 
     @Bean
-    public Docket createRestApi() {
+    public Docket Api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+//                .pathMapping("/school")
+                .apiInfo(apiInfo())
+                .enable(true);
     }
+
+
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("设备维护接口文档")
-                .description("设备维护接口文档")
-                .termsOfServiceUrl("http://www.wqewt.com:2443")
-                .version("1.0")
+                .title("swagger2")
+                .contact("xiaoying")
+                .description("</br>" +
+                        "one" +
+                        "</br>"
+                )
+                .version("0.1.1")
                 .build();
     }
-
 }
